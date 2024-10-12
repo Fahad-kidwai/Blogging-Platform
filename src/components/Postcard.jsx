a0eb2f191b199c669136364da3133a7a4cdefdfa
fahad-cloudfort <fahad@i.cloudfort.in>
2024-10-12 16:59:22 +0530

    String handled

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -3,6 +3,11 @@ import appwriteService from "../appwrite/config";
 import { Link } from "react-router-dom";
 
 function Postcard({ $id, title, featuredImage }) {
+  console.log(title.length, "Title Length");
+  console.log(
+    title.length > 30 ? title.substring(0, 29) + "..." : title,
+    "shorting"
+  );
   return (
     <Link to={`/post/${$id}`}>
       <div className="w-full bg-gray-900 text-[#FFFDF2] rounded-xl p-4">
@@ -13,7 +18,9 @@ function Postcard({ $id, title, featuredImage }) {
             className="rounded-xl"
           />
         </div>
-        <h2 className="text-xl font-bold">{title}</h2>
+        <h2 className="text-xl font-bold">
+          {title.length > 30 ? title.substring(0, 29) + "..." : title}
+        </h2>
       </div>
     </Link>
   );
